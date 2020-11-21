c8b7b148097d99e5e258773904edfca02295b177
theking465 <9497296+theking465@users.noreply.github.com>
2020-11-21 21:25:12 +0100

    added current github repo

--- src/components/Github.js
+++ src/components/Github.js
@@ -7,7 +7,9 @@ export default class Github extends Component {
         return (
             <div className="github">
                 <div className="line line2"></div>
-                Original repo by <a href="https://github.com/thomasdondorf/poke-level-calc">Thomas Dondorf</a>
+                Original repo by <a href="https://github.com/thomasdondorf/poke-level-calc">Thomas Dondorf</a> <br></br>
+                Current repo by <a href="https://github.com/theking465/poke-level-calc">theking465</a>
+                
             </div>
         );
     }
